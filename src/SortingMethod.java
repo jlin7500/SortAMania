d5baa62ace54844830aa8560c1bfc21df882c2ae
BT_1N3_25 <sjiang9453@bths.edu>
2018-11-28 10:00:45 -0500

    2 SortingMethod Implemented

--- src/SortingMethod.java
+++ src/SortingMethod.java
@@ -1,21 +1,39 @@
 public class SortingMethod
 {
-        public static void bubbleSort(String[] args)
+    public static void bubbleSort(String[] arr)
+    {
+        for( int i = 0; i < arr.length; i++)
         {
-            int i = 0;
-            int[] arr = new int[10000];
-            boolean swap = true;
-            while(swap == true)
+            if( arr[i].compareTo(arr[i+1]) > 0)
             {
-                i++;
-                arr[i] = (int)Math.random() * 10000;
-                if(arr[i] > arr[i+1])
-                {
-                    int blank = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = blank;
-                    swap = true;
-                }
+                String bubble = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = bubble;
             }
         }
+    }
+
+    public static void selectionSort(int[] arr)
+    {
+        int swap = 0;
+        double min = 0;
+
+        for ( int i =0; i< arr.length; i++)
+        {
+            swap = i;
+            min = arr[i];
+
+            for( int j = i ; j < arr.length ; j++)
+            {
+                if ( arr[j] < min)
+                {
+                    swap = j;
+                    min = arr[j];
+                }
+            }
+            int x = arr[i];
+            arr[i] = arr [swap];
+            arr[swap] = x;
+        }
+    }
 }
