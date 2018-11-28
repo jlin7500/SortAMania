815ee8e6c595e908597ab7899e8d9065dcfd251f
BT_1N3_25 <sjiang9453@bths.edu>
2018-11-28 09:53:44 -0500

    Initial Commit

---
+++ src/Runner.java
@@ -0,0 +1,7 @@
+public class Runner
+{
+    public static void main( String [] args)
+    {
+        SortCompetition [] num = new SortCompetition[10000];
+    }
+}
---
+++ src/SortingMethod.java
@@ -0,0 +1,4 @@
+public class SortingMethod
+{
+    public void bubbleSort
+}
